81af378714cbc3023d3f7a6bcc05655f20316eff
Julien Bloit <julien.bloit@gmail.com>
2021-05-23 16:45:02 +0200

    reichstar with correct blending effect in app

---
+++ reichStarPaintCode/PCView.h
@@ -0,0 +1,16 @@
+//
+//  PCView.h
+//  reichStarPaintCode
+//
+//  Created by Julien Bloit on 21/05/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PCView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ reichStarPaintCode/PCView.m
@@ -0,0 +1,22 @@
+//
+//  PCView.m
+//  reichStarPaintCode
+//
+//  Created by Julien Bloit on 21/05/2021.
+//
+
+#import "PCView.h"
+#import "ReichStarStyleKit.h"
+
+@implementation PCView
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+    [ReichStarStyleKit drawReichStarWithFrame:rect resizing:ReichStarStyleKitResizingBehaviorAspectFill angle:20];
+}
+
+
+@end
